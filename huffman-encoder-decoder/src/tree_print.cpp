e4140e6d8f60b2052ffde0090695186bb954da4e
HEX9CF <hex9cf@aliyun.com>
2024-05-22 23:26:15 +0800

    添加将字符形式的哈夫曼树存于文件 TreePrint 中。

--- huffman-encoder-decoder/src/tree_print.cpp
+++ huffman-encoder-decoder/src/tree_print.cpp
@@ -5,29 +5,38 @@
 将已在内存中的哈夫曼树以直观的方式（树或凹入表形式）显示在终端上，同时将此字符形式的哈夫曼树写入文件TreePrint中。
 */
 
+static ofstream fout;
+ 
 // 遍历哈夫曼树
 void traverse(HuffmanTree T, int d, string c) {
 	if (T) {
 		if (!d) {
 			cout << "root" << endl;
+			fout << "root" << endl;
 		} else if (T->data == ' ') {
 			cout << c << endl;
+			fout << c << endl;
 		}
 		else {
 			cout << T->data << ": " << c << endl;
+			fout << T->data << ": " << c << endl;
 		}
 		if (T->left) {
 			for (int i = 0; i < d; i++) {
 				cout << "  ";
+				fout << "  ";
 			}
 			cout << "└─";
+			fout << "  "; 
 			traverse(T->left, d + 1, c + "0");
 		}
 		if (T->right) {
 			for (int i = 0; i < d; i++) {
 				cout << "  ";
+				fout << "  ";
 			}
 			cout << "└─";
+			fout << "  "; 
 			traverse(T->right, d + 1, c + "1");
 		}
 	}
@@ -45,7 +54,19 @@ Status treePrint() {
 		return ERROR;
 	}
 
+	// 打开文件
+	fout = ofstream("TreePrint", ios::out);
+	if (!fout) {
+		cerr << "发生错误：无法打开文件 TreePrint 。" << endl;
+		system("pause");
+		return ERROR;
+	}
+
+	// 遍历哈夫曼树
 	traverse(huffmanTree, 0, "");
+	cout << "\n" << "已将字符形式的哈夫曼树存于文件 TreePrint 中。" << endl;
+
+	fout.close();
 	system("pause");
 	return OK;
 }
